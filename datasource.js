dcc8b317b46dd56623ea08c676cbcd1bbfad694b
zhiqiu <zhiqiu@berkeley.edu>
2019-08-04 12:57:04 -0700

    finished barebone of quiz project

---
+++ datasource.js
@@ -0,0 +1,72 @@
+const STORE = [
+    {
+        question: "What year was Netflix created?",
+        options: [1997, 1999, 2001, 2003, 2005],
+        answer: 1997,
+        icon: 'https://usercontent1.hubstatic.com/14462478.gif',
+        alt: "Year 1997"
+    },
+    {
+        question: "How many minutes do the average Netflix subscriber stream a day?",
+        options: [30, 60, 90, 120, 150],
+        answer: 90,
+        icon: 'http://bnisouthcheshire.co.uk/images/90-minutes.png',
+        alt: "90 Minutes"
+    }, 
+    {
+        question: "What's the percentage of Netflix users who binge-watch shows at least every few weeks?",
+        options: [20, 40, 60, 80, 100],
+        answer: 60,
+        icon: 'https://maatitv.com/wp-content/uploads/2018/12/Bingewatch-e1520346418947.jpg',
+        alt: "Binge Watch till 4:30am, play next episode?"
+    }, 
+    {
+        question: "What is the most watched show in Netflix?",
+        options: ["House of Cards", "The Office", "Parks and Recreation", "Breaking Bad", "Family Guy"],
+        answer: "Breaking Bad",
+        icon: 'https://static0.srcdn.com/wordpress/wp-content/uploads/2019/01/Breaking-Bad-Movie-Characters.jpg.png',
+        alt: "Breaking Bad show poster"
+    }, 
+    {
+        question: "What other names were the founders of Netflix considering for their company?",
+        options: ["flix.com", "moviesrus.com", "rentals.com", "disc.com", "kibble.com"],
+        answer: "kibble.com",
+        icon: 'https://cdn.hinative.com/attached_images/137515/f0eb43759cd2e798830f557e34e6ddf32b04f94d/large.jpg',
+        alt: "bowl of kibbles"
+    }, 
+    {
+        question: "What was the first Netflix-original show?",
+        options: ["The Show", "House of Cards", "Hemlock Grove", "Orange Is the New Black", "Marco Polo"],
+        answer: "House of Cards",
+        icon: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/House_of_Cards_season_1.png/250px-House_of_Cards_season_1.png',
+        alt: "house of cards poster"
+    }, 
+    {
+        question: "How many membership plans do Netflix have?",
+        options: [1, 2, 3, 4, 5],
+        answer: 3,
+        icon: 'https://assets.pcmag.com/media/images/558788-netflix-plans.png?thumb=y&width=500',
+        alt: "Netflix Subscription Plan"
+    }, 
+    {
+        question: "In 2018, how many movies and TV shows are in the Netflix streaming catalog??",
+        options: [5500, 6000, 6500, 7000, 8000],
+        answer: 5500,
+        icon: 'https://qph.fs.quoracdn.net/main-qimg-0891d56447bafbd96aee1a4b3a06e8bd',
+        alt: "Statistics on the number of movies and TV shows in Netflix, total is 5579 in 2018 for US."
+    }, 
+    {
+        question: "Where in the world is Netflix not available?",
+        options: ["Australia", "India", "Hong Kong", "China", "Africa"],
+        answer: "China",
+        icon: 'https://help.nflxext.com/7f54106e-9ad0-4cd4-9f87-84681ae33fd4_helloWorld_en.png',
+        alt: "World Map of all the countries netflix is available"
+    },
+    {
+        question: "In how many countries is Netflix available?",
+        options: [150, 190, 230, 270, 300],
+        answer: 190,
+        icon: 'https://help.nflxext.com/7f54106e-9ad0-4cd4-9f87-84681ae33fd4_helloWorld_en.png',
+        alt: "World Map of all the countries netflix is available."
+    }
+];
--- index.js
+++ index.js
@@ -1,33 +1,72 @@
+// When you start the quiz, the flow of the function is the following: 
+// startQuiz, renderQuestion, userAnswer, checkAnswer, correct or incorrectPrompt, renderNextQuestion, either loops back to renderQuestion or ends with the renderResults. 
+
 let questionNum = 0;
 let score = 0;
 
-function getQuestions () {
-
-}
-
-function startQuestion () {
-
-}
-
-function nextQuestion () {
-
-}
-
-function lastQuestion () {
-    // end script
+function renderQuestion () {
+    addQuestionNum();
+    $('.questions').html(`<div class="question-${questionNum}"> ${STORE[questionNum-1].question} 
+    <form>
+    <fieldset>
+    <label class="questionOptions"> 
+    <input type="radio" value="${STORE[questionNum-1].options[0]}" name="answer" required>
+    <span>${STORE[questionNum-1].options[0]}</span>
+    </label>
+    <label class="questionOptions"> 
+    <input type="radio" value="${STORE[questionNum-1].options[1]}" name="answer" required>
+    <span>${STORE[questionNum-1].options[1]}</span>
+    </label>
+    <label class="questionOptions"> 
+    <input type="radio" value="${STORE[questionNum-1].options[2]}" name="answer" required>
+    <span>${STORE[questionNum-1].options[2]}</span>
+    </label>
+    <label class="questionOptions"> 
+    <input type="radio" value="${STORE[questionNum-1].options[3]}" name="answer" required>
+    <span>${STORE[questionNum-1].options[3]}</span>
+    </label>
+    <label class="questionOptions"> 
+    <input type="radio" value="${STORE[questionNum-1].options[4]}" name="answer" required>
+    <span>${STORE[questionNum-1].options[4]}</span>
+    </label>
+    <button type="submit" class="submitButton">Submit</button>
+    </fieldset>
+    </form>
+    </div> 
+    `)
+    userAnswer();
 }
 
 function userAnswer () {
-
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+        let userSelect = $('input:checked').attr('value');
+        checkAnswer(userSelect);
+    })
 }
 
-function isCorrect () {
-    addScore();
-    $('.score').text(score);
+function checkAnswer(selected) {
+    let answer = STORE[questionNum-1].answer;
+    if (!(isNaN(answer))) {
+        selected = Number(selected);
+    }
+    if (selected === answer) {
+        addScore();
+        $('.js-score').text(score);
+        correctPrompt();
+    } else {
+        incorrectPrompt();
+    }
 }
 
-function isWrong () {
-    // tell them it's wrong and give them the correct answer
+function correctPrompt () {
+    $('.questions').html(`<div class="resultPrompt"><div class="icon"><img src="${STORE[questionNum-1].icon}" alt="${STORE[questionNum-1].alt}"/></div><p>You got it right!</p><button type=button class="nextButton">Next</button></div>`);
+    renderNextQuestion();
+}
+
+function incorrectPrompt () {
+    $('.questions').html(`<div class="resultPrompt"><div class="icon"><img src="${STORE[questionNum-1].icon}" alt="${STORE[questionNum-1].alt}"/></div><p>You got it wrong! The correct answer was ${STORE[questionNum-1].answer}.</p><button type=button class="nextButton">Next</button></div>`);
+    renderNextQuestion();
 }
 
 function addScore () {
@@ -36,26 +75,50 @@ function addScore () {
 
 function addQuestionNum () {
     questionNum++;
+    $('.js-question-num').text(questionNum);
 }
 
-function renderQuestion () {
-    startQuestion();
-
-}
-
-function renderResults () {
-    
-}
-
-function startQuiz () {
-    (".main-content").on("click", '.startButton', function(event) {
-        $(".begin").remove;
+function renderNextQuestion () {
+    $('.nextButton').on('click', function(event) {
+        if (questionNum >= STORE.length) {
+            renderResults();
+            restartQuiz();
+        } else {
+            $('.correctPrompt').remove();
+            renderQuestion();
+        }
     });
 }
 
+function renderResults () {
+    console.log('ending results');
+    if (score >= 8) {
+        $('.main-Content').html(`<div class='results'>You're a Netflix Pro!</div> <button class='restartButton'>Restart Quiz</button></div>`)
+    } else if (score < 8 && score >= 5) {
+        $('.main-Content').html(`<div class='results'>You're basic!</div> <button class='restartButton'>Restart Quiz</button></div>`)
+    } else {
+        $('.main-Content').html(`<div class='results'>Your Netflix level is elementary.</div> <button class='restartButton'>Restart Quiz</button></div>`)
+    }   
+}
+
+function restartQuiz () {
+    $(".main").on("click", "restartButton", function (){
+        location.reload();
+    });
+}
+
+// When you start the Quiz you would remove the content in the center and leave the title. 
+function startQuiz () {
+    $(".main-content").on('click', '.startButton', function(event) {
+        $(".begin").remove();
+        $(".game-levels").remove();
+        renderQuestion();
+    });
+}
+
+// High level function that allows for the questions to continue rolling. 
 function runQuiz () {
     startQuiz();
-    renderQuestion();
 }
 
 $(runQuiz);
